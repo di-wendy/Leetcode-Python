fd14b382d5f6c648a02596f9a5fa4cb118909e47
Di Wang <jiankong3@gmail.com>
2016-01-17 22:23:21 -0500

    Update 328 Odd-even-linked-list.py

--- 328 Odd-even-linked-list.py
+++ 328 Odd-even-linked-list.py
@@ -1,4 +1,18 @@
 
+'''Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Example:
+Given 1->2->3->4->5->NULL,
+return 1->3->5->2->4->NULL.
+
+Note:
+The relative order inside both the even and odd groups should remain as it was in the input. 
+The first node is considered odd, the second node even and so on ...'''
+
+
+
 class Solution(object):
     def oddEvenList(self, head):
         """
@@ -6,12 +20,12 @@ class Solution(object):
         :rtype: ListNode
         """
         if head:
-            odd_tail, cur = head, head.next
-            while cur and cur.next:
-                even_head = odd_tail.next
-                odd_tail.next = cur.next
-                odd_tail = odd_tail.next
-                cur.next = odd_tail.next
-                odd_tail.next = even_head
-                cur = cur.next
+            last_ji, record = head, head.next   
+            while record and record.next:
+                first_ou = last_ji.next
+                last_ji.next = record.next
+                last_ji = last_ji.next
+                record.next = last_ji.next
+                last_ji.next = first_ou
+                record = record.next
         return head
