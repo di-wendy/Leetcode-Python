1e1a743d22eb9d17e754b6801ea4af644bb60f13
Di Wang <jiankong3@gmail.com>
2016-09-01 00:01:06 -0400

    Update 58 Length of Last Word.py

--- 58 Length of Last Word.py
+++ 58 Length of Last Word.py
@@ -1,1 +1,11 @@
-will
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s.count(" ")==len(s):
+            return 0
+        result = s.split()
+        
+        return len(result[len(result)-1])
