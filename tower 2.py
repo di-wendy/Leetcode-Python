674586fe65e748c7af75c3dce26395fdaa612f9e
Di Wang <jiankong3@gmail.com>
2016-09-28 21:05:01 -0400

    Create tower 2.py

---
+++ tower 2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 20:54:07 2016
+
+@author: Dwang
+
+Given:
+Plaintext to encrypt
+A key defined by a square matrix
+
+加密过程：
+1. 首先将所有的转换成大写
+2. 去掉字符 Hi!there  -> HITHERE
+3. 搜索矩阵，将#换成字母，从左到右换
+4. 读取矩阵，从上往下读，读出以后，输出五字母一组
+5. 如果不能完全解密，从头开始
+
+"""
+
+
+
